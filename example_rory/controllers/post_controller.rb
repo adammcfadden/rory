b31c1f4e71710195a7c7b479c27b60b0d946a440
Adam <amcfadden@renewfund.com>
2015-08-06 15:23:46 -0700

    RORY EX: add json fixture for testing, set a response in the post#index action that returns a json when curled

--- example_rory/controllers/post_controller.rb
+++ example_rory/controllers/post_controller.rb
@@ -3,5 +3,9 @@ class PostController < ApplicationController
   # define actions here, these actions will link to your route definitions.
   # def index within the post controller will map to 'post#index'
   def index
+    # set contractors to equal a json file stored in the fixtures folder
+    contractors = File.read(File.join('spec', 'fixtures', 'contractors.json'))
+    # set the @response to send json content type and the contractors file
+    @response = [200, {'Content-Type' => 'application/json'}, [contractors]]
   end
 end
